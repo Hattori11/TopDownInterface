3f8a92e73c62d22f2bd18753205de7458bf78485
Guilherme Rocha <guirc6@gmail.com>
2025-11-17 16:32:11 -0300

    Interface inicial concluída (HTML e CSS)

---
+++ assets/js/createProblem.js
@@ -0,0 +1,30 @@
+// Template
+<li>
+  <div class="problem-header">
+    <button class="open-or-closed">
+      <i class="fa-solid fa-chevron-down"></i>
+    </button>
+    <div class="problem-name">segundo exemplo de problema hello word</div>
+    <div class="status-pending">
+      <span class="status">pendente</span>
+    </div>
+    <button class="add-subproblem">
+      <i class="fa-solid fa-plus"></i>
+    </button>
+    <button class="open-opition">
+      <i class="fa-solid fa-ellipsis"></i>
+    </button>
+    <dialog class="option hidden">
+      <button class="remove" aria-label="Remover problema">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+      <button class="edit" aria-label="Editar problema">
+        <i class="fa-solid fa-pen"></i>
+      </button>
+      <button class="add-function">
+        <i class="fa-solid fa-square-plus"></i>
+        <span>Adicionar função</span>
+      </button>
+    </dialog>
+  </div>
+</li>
