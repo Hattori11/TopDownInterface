0480bdd7d7ea5de07b6ac9f19cc45b9a22289de4
Guilherme Rocha <guirc6@gmail.com>
2025-12-15 17:29:26 -0300

    function addProblem

--- js/addProblem.js
+++ js/addProblem.js
@@ -1,11 +1,28 @@
 const modal = document.getElementById("add-problem");
 const open = document.querySelector("section#container-problem > div.container-add");
 const closed = document.getElementById("closed-modal");
+const input = document.querySelector("dialog#add-problem > form > input");
+const containerProblem = document.getElementById("container-problem");
 
 open.addEventListener("click", () => {
   modal.showModal();
 });
 
-closed.addEventListener("click", () => {
+closed.addEventListener("click", (event) => {
+  event.preventDefault();
+  
+  const level0 = document.createElement("div");
+  level0.setAttribute("class", "level level-0");
+  const problem = document.createElement("div");
+  problem.setAttribute("class", "node");
+
+  problem.textContent = input.value;
+
+  level0.appendChild(problem);
+  containerProblem.appendChild(level0);
+
+  open.style.display = "none";
+  containerProblem.style.justifyContent = "flex-start";
+
   modal.close();
 });
---
+++ js/addSubProblems.js
@@ -0,0 +1,64 @@
+
+
+/*
+    <style>
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+      }
+      html {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      body {
+        height: 90%;
+        width: 90%;
+        border: solid 1px #000;
+        padding: 5px;
+      }
+      .tree {
+        width: 100%;
+      }
+
+      .level {
+        display: flex;
+        width: 100%;
+        margin: 20px 0;
+      }
+
+      .node {
+        flex: 1;
+        min-width: 80px;
+        height: 50px;
+        border: 1px solid #000;
+        margin: 0 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="tree">
+      <div class="level level-0">
+        <div class="node">Root</div>
+      </div>
+
+      <div class="level level-1">
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+      </div>
+
+      <div class="level level-2">
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+        <div class="node"></div>
+      </div>
+    </div>
+  </body>
+*/
